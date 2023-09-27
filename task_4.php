17fcfd39fb56001492ee8c4a88c1f48e0bb2bd92
Arhan Islam <arhanislam3407@gmail.com>
2023-09-27 22:10:34 +0600

    Assignment Submission

---
+++ task_4.php
@@ -0,0 +1,17 @@
+<?php
+$studentGrades = array(
+    'Student 1' => array('Math' => 85, 'English' => 92, 'Science' => 78),
+    'Student 2' => array('Math' => 88, 'English' => 95, 'Science' => 90),
+    'Student 3' => array('Math' => 75, 'English' => 80, 'Science' => 88)
+);
+function calculateAndPrintAverages($studentGrades) {
+    foreach ($studentGrades as $student => $grades) {
+        $totalGrades = array_sum($grades);
+        $averageGrade = $totalGrades / count($grades);
+
+        echo "Candidate: $student" .PHP_EOL;
+        echo "Average Grade: $averageGrade" . "\n";
+    }
+}
+calculateAndPrintAverages($studentGrades);
+?>
